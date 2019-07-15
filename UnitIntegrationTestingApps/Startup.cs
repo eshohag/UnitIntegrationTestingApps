b13b0ba927bbcd9c0a7b19d4b23bf35e33afe4b9
Md. Shohag Mia <shohag.mia@datasoft-bd.com>
2019-07-15 13:47:21 +0600

    Add project files.

---
+++ UnitIntegrationTestingApps/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UnitIntegrationTestingApps.Startup))]
+namespace UnitIntegrationTestingApps
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
